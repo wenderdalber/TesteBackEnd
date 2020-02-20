022d4ee824f320810396b4d024875b9ac0e4a57d
Wender Dalber <wenderdalber@gmail.com>
2020-02-19 22:31:00 -0300

    Criacao do teste unitario de validacao de media final do aluno

--- AppAlunosMedia.Tests/UnitTest1.cs
+++ AppAlunosMedia.Tests/UnitTest1.cs
@@ -4,15 +4,25 @@ namespace Tests
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
+        private readonly IServiceMedia _serviceMedia;
+
+        public Tests(IServiceMedia serviceMedia)
         {
+            _serviceMedia = serviceMedia;
         }
 
         [Test]
-        public void Test1()
+        public void Dado_Nota_Menor_Que_sete_Reprovado()
         {
-            Assert.Pass();
+            _serviceMedia.ValidateMedia();
+            Assert.IsTrue(true);
+        }
+
+        [Test]
+        public void Dado_Nota_Maior_Que_sete_Aprovado()
+        {
+            _serviceMedia.ValidateMedia();
+            Assert.IsTrue(false);
         }
     }
 }
