452d1afc5fe8a3f1cb12358001ddc8a940948c7c
Wender Dalber <wenderdalber@gmail.com>
2020-02-19 22:33:36 -0300

    Criacao da interface MVC para visualizar e lista alunos, com filtros

---
+++ AppAlunosMedia/Controllers/ValuesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AppAlunosMedia.Application.Interface;
+using AppAlunosMedia.Domain.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppAlunosMedia.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        private readonly IJobs _jobs;
+
+        public ValuesController(IJobs jobs)
+        {
+            _jobs = jobs;
+        }
+
+        [Route("api/v1/create")]
+        [HttpPost]
+        public string Create([FromBody]Student student)
+        {
+            student.Status = "notread";
+            _jobs.Create(student);
+            return "Ok";
+        }
+
+        [Route("api/v1/update")]
+        [HttpPut]
+        public string Update([FromBody]Student student)
+        {
+            _jobs.Update(student);
+            return "Ok";
+        }
+
+        [Route("api/v1/delete")]
+        [HttpDelete]
+        public string Delete(int RA)
+        {
+            _jobs.Delete(RA);
+            return "Ok";
+        }
+
+        [Route("api/v1/list")]
+        [HttpGet]
+        public IEnumerable<Student> GetAll()
+        {
+            return _jobs.GetAll();
+        }
+    }
+}
---
+++ AppAlunosMedia/wwwroot/js/app.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function () {
+    CarregarDados();
+});
+
+
+function CarregarDados() {
+    $.ajax({
+        type: "GET",
+        url: '/api/v1/list',
+        data: {},
+        dataType: "json",
+        success: function (data) {
+            //alert()
+            //var d = JSON.parse(data);
+            if (data.length > 0) {
+                var comecoTabela = "<table class='table table-bordered table-hover'><tr><td>RA</td><td>Name</td><td>Period</td><td>Course</td><td>Note</td><td>Status</td></tr><tbody>";
+                var meioTabela = "";
+                for (var x = 0; x < data.length; x++) {
+                    meioTabela = meioTabela + "<tr><td style='text-align: center'></td><td>" + d[x].RA + "</td><td>" + data[x].Name + "</td><td>" + d[x].Period + "</td><td>" + d[x].Course + "</td><td>" + d[x].Note + "</td><td>" + d[x].Status + "</td>";
+                }
+                var finalTabela = "</tboby></table>";
+                $("#tabelaPrincipal").html(comecoTabela + meioTabela + finalTabela);
+            } else {
+                $("#mensagemFinalPrincipal").append("<span>Nenhuma assinatura no período</span>");
+            }
+        }
+    }
